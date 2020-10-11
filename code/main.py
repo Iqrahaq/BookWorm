3860c254dca675f156d862646b1556ad9bf409ea
Iqra <iqrahaq@live.co.uk>
2020-10-11 22:32:50 +0100

    updated

--- code/main.py
+++ code/main.py
@@ -4,6 +4,9 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import asyncio
+import csv
+import time
 
 # Use dotenv to conceal token.
 load_dotenv()
@@ -38,9 +41,25 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
+
+# Set custom status for bot.
+async def custom_status():
+    while True:
+        with open('books.csv', 'r') as books_file:
+            books = csv.reader(books_file)
+            next(books_file)
+            for book in books:
+                if any(book):
+                    book_name = book[0]
+                    book_authors = book[1]
+                    book_url = book[5]
+                    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, state="Listening", name="'{0}' by {1}. 🎧".format(book_name, book_authors)))
+                    await asyncio.sleep(15000)
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    client.loop.create_task(custom_status())
 
 # token
 client.run(TOKEN, reconnect=True)
